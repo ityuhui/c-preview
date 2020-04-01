d07610a833ca849a80b3d0c1a0cbf519d8ab45bc
Hui Yu <ityuhui@gmail.com>
2020-04-01 23:09:07 +0800

    Improve the code quality on Apri 1,2020

--- kubernetes/config/kube_config_yaml.h
+++ kubernetes/config/kube_config_yaml.h
@@ -9,12 +9,17 @@ extern "C" {
 #endif                          /* __cplusplus */
 
 /*
- * kubeyaml_loadkubeconfig
+ * kubeyaml_load_kubeconfig
  *
  * Description:
  *
  * Load the kubeconfig file specified by kubeconfig->fileName
  * and get the kuberntes configuration for other fields of kubeconfig.
+ *
+ * Return:
+ *
+ *   0     Success
+ *  -1     Failed
  *
  * Parameter:
  *
@@ -25,6 +30,31 @@ extern "C" {
  * kubeconfig: kubernetes cluster configuration
  */
 extern int kubeyaml_load_kubeconfig(kubeconfig_t * kubeconfig);
+
+
+/*
+ * kubeyaml_free_kubeconfig
+ *
+ *
+ * Description:
+ *
+ * Free the memory for the struct kubeconfig_t
+ *
+ *
+ * Return:
+ *
+ * None
+ * 
+ * 
+ * Parameter:
+ *
+ * IN:
+ * kubeconfig: The pointer to the kubernetes cluster configuration 
+ *
+ * OUT:
+ * None
+ *
+ */
 
 extern void kubeyaml_free_kubeconfig(kubeconfig_t * kubeconfig);
 
