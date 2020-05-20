019f6e6942b6c3c02309d01df47dd3d2f56986ea
Hui Yu <ityuhui@gmail.com>
2020-05-20 20:41:57 +0800

    update exec provider example

--- examples/exec_provider/my_exec_provider.c
+++ examples/exec_provider/my_exec_provider.c
@@ -27,15 +27,15 @@ char certificate_template[] = "\
 
 int main(int argc, char *argv[])
 {
-    const char *clientCertificateData = secure_getenv(ENV_EXEC_CLIENT_CERTIFICATE_DATA);
-    const char *clientPrivateKey = secure_getenv(ENV_EXEC_CLIENT_PRIVATE_KEY);
+    const char *client_certificate_data = secure_getenv(ENV_EXEC_CLIENT_CERTIFICATE_DATA);
+    const char *client_private_key = secure_getenv(ENV_EXEC_CLIENT_PRIVATE_KEY);
 
     if ((4 == argc) && argv[3]) {
         // token is passed by command line argument
         printf(token_template, argv[3]);
-    } else if ((clientCertificateData) && strlen(clientCertificateData) > 0 && (clientPrivateKey) && strlen(clientPrivateKey) > 0) {
+    } else if ((client_certificate_data) && strlen(client_certificate_data) > 0 && (client_private_key) && strlen(client_private_key) > 0) {
         // client certificate and private key are passed by environment variables
-        printf(certificate_template, clientCertificateData, clientPrivateKey);
+        printf(certificate_template, client_certificate_data, client_private_key);
     } else {
         printf("Cannot get authentication data\n");
     }
