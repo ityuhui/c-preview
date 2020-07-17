b458b3250dda0353c26dd88b5cdb72a233b07d83
Hui Yu <ityuhui@gmail.com>
2020-07-17 16:47:16 +0800

    2020-7-17 Fri, expired function in oidc plugin

--- kubernetes/config/authn_plugin/plugins/oidc/libkubernetes_oidc.c
+++ kubernetes/config/authn_plugin/plugins/oidc/libkubernetes_oidc.c
@@ -1,39 +1,67 @@
 #include "authn_plugin.h"
 #include "simple_http_client.h"
+#include "binary.h"
+#include "cJSON.h"
+
+#define ID_TOKEN_EXP "exp"
 
 static time_t get_token_expiration_time(const char *token_string)
 {
+    static char fname[] = "get_token_expiration_time()";
+
+    time_t expiration_time = 0;
+
     if (!token_string) {
         return 0;
     }
-    char* string = strdup(token_string);
-    if (!string) {
+    char *dup_token_string = strdup(token_string);
+    if (!dup_token_string) {
         return 0;
     }
 
-    char* p = NULL:
-    p = strtok(string, "."); /* jwt header */
+    char *p = NULL:
+    p = strtok(dup_token_string, "."); /* jwt header */
     if (!p) {
-        goto error;
+        goto end;
     }
     p = strtok(NULL, ".");  /* jwt payload */
     if (!p) {
-        goto error;
+        goto end;
     }
-    base64decode(p);
 
-    char *pay_load = get_jwt_payload(token_string);
-    
-    get_token_expiration_time
+    int decoded_bytes = 0;
+    char* b64decode = base64decode(p, strlen(p), &decoded_bytes);
+    if (!b64decode || 0 == decoded_bytes) {
+        fprintf(stderr, "%s: Base64 decodes failed.\n", fname);
+        goto end;
+    }
 
-    
+    cJSON *payload_JSON = cJSON_Parse(b64decode);
+    if (!payload_JSON) {
+        fprintf(stderr, "%s: Cannot create JSON from string.[%s].\n", fname, cJSON_GetErrorPtr());
+        goto end;
+    }
+    json_value = cJSON_GetObjectItem(payload_JSON, ID_TOKEN_EXP);
+    if (json_value->type != cJSON_Number) {
+        fprintf(stderr, "%s: Cannot get expiration time in id token.\n", fname);
+        goto end;
+    }
+    expiration_time = json_value->valueint;
 
-    freejwt(&jwt);
-    free(string)
-    return 0;
-error:
-    free(string);
-    return -1;
+end:
+    if (payload_JSON) {
+        cJSON_Delete(payload_JSON);
+        payload_JSON = NULL;
+    }
+    if (b64decode) {
+        free(b64decode);
+        b64decode = NULL;
+    }
+    if (dup_token_string) {
+        free(dup_token_string);
+        dup_token_string = NULL;
+    }
+    return expiration_time;
 }
 
 char* get_token(kubeconfig_property_t* auth_provider)
