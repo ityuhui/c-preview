e1f841e3e4a250344291f915f147164aad3475c0
Hui Yu <ityuhui@gmail.com>
2020-07-27 10:59:01 +0800

    10

--- kubernetes/config/kube_config_util.h
+++ kubernetes/config/kube_config_util.h
@@ -5,9 +5,50 @@
 extern "C" {
 #endif
 
-char* kubeconfig_mk_cert_key_tempfile(const char*);
+/*
+ * kubeconfig_mk_cert_key_tempfile
+ *
+ * Description:
+ *
+ * Create a temporary file to persist SSL/TLS certificate or key
+ *
+ * Return:
+ *
+ *  char *   :   File name of created temporary file
+ *  NULL     :   Failed to create temporary file
+ *
+ * Parameter:
+ *
+ * IN:
+ * data: raw data of SSL/TLS certificate or key
+ *
+ * OUT:
+ * None
+ *
+ */
+    char* kubeconfig_mk_cert_key_tempfile(const char *data);
 
-void kubeconfig_rm_tempfile(const char*);
+/*
+ * kubeconfig_rm_tempfile
+ *
+ * Description:
+ *
+ * Delete the temporary file
+ *
+ * Return:
+ * 
+ * None
+ *
+ * Parameter:
+ *
+ * IN:
+ * filename: The file name of temporary file
+ *
+ * OUT:
+ * None
+ *
+ */
+    void kubeconfig_rm_tempfile(const char *filename);
 
 #ifdef  __cplusplus
 }
